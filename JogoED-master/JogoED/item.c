8bd8b6c0c3fe644ad91707e153c9c8d8c9e949aa
Wesley <wsl123@bol.com.br>
2018-05-23 17:41:50 -0300

    Initial commit

---
+++ JogoED-master/JogoED/item.c
@@ -0,0 +1,86 @@
+#include "item.h"
+#include <allegro5\allegro.h>
+#include "personagem.h"
+
+void verificaPegouItem(personagem p, lista *pegar, lista *inventario) {
+	no *aux = *pegar;
+	bool pegou = false;
+	while ((aux != NULL) && (pegou == false)) {	
+		if ((aux->objeto.mX == p.x) && (aux->objeto.mY == p.y)) {
+			no *aux2 = aux;
+			removerNo(pegar, aux);
+			adicionaNo(inventario, aux2);
+			pegou = true;
+		}
+		else aux = aux->prox;
+	}
+}
+void removerNo(lista *l, no *n) {
+	no *aux = *l;
+	if (n == *l) { //Primeiro da lista
+	*l = aux->prox;
+	}
+	else {
+		bool removeu = false;
+		no* aux = *l;
+		while(aux != NULL && aux->prox != n){
+			aux = aux->prox;
+		}
+		aux->prox = n->prox;
+	}
+	while (aux->prox != NULL) {
+		aux->prox->objeto.bX -= 55;
+		aux = aux->prox;
+	}
+}
+
+
+void moveItens(lista *pegar, int x, int y) {
+	no *aux = *pegar;
+	while (aux != NULL) {
+		aux->objeto.cX += x;
+		aux->objeto.cY += y;
+		aux = aux->prox;
+	}
+}
+
+void adicionaNo(lista *l, no *n) {
+	no *aux = n;
+	aux->objeto.bX = posXInvetario;
+	aux->objeto.bY = posYInvetario;
+	posXInvetario += 55;
+
+	if (*l == NULL) {
+		aux->prox = NULL;
+		*l = aux;
+	}
+	else {
+		aux = *l;
+		while (aux->prox != NULL) 
+			aux = aux->prox;
+		
+		n->prox = aux->prox;
+		aux->prox = n;
+	}
+
+}
+
+void mostraListaInventario(lista *l) {
+	no *aux = *l;
+	if (l != NULL) {
+		while (aux != NULL) {
+			al_draw_bitmap(aux->objeto.item, aux->objeto.bX, aux->objeto.bY, 0);
+			aux = aux->prox;
+		}
+	}
+}
+
+void mostraItemMapa(lista *l) {
+	no *aux = *l;
+	if (l != NULL) {
+		while (aux != NULL) {
+			al_draw_bitmap(aux->objeto.item, aux->objeto.cX, aux->objeto.cY, 0);
+			aux = aux->prox;
+		}
+	}
+}
---
+++ JogoED-master/JogoED/item.h
@@ -0,0 +1,39 @@
+#include <allegro5\allegro.h>
+#include "personagem.h"
+#ifndef ITEM_H
+#define ITEM_H
+
+typedef struct obj { 
+	//Coordenada mapa;
+	int cY;
+	int cX;
+	//posição na matriz
+	int mY;
+	int mX;
+	//Coordenada x,y (em px) para borda
+	int bY;
+	int bX;
+	ALLEGRO_BITMAP *item;
+
+}obj;
+
+static int posXPegar = 355;
+static int posYPegar = 430;
+static int posXInvetario = 475;
+static int posYInvetario = 535;
+typedef struct no {
+	obj objeto;
+	struct no* prox;
+}no;
+
+typedef no* lista;
+
+void verificaPegouItem(personagem p, lista *pegar, lista *inventario);
+void removerNo(lista *l, no *n);
+void adicionaNo(lista *l, no *n);
+void moveItens(lista *pegar, int x, int y);
+void mostraItemMapa(lista *l);
+void mostraListaInventario(lista *l);
+
+
+#endif // !ITEM_H
